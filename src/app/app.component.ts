7f6e28985eba49ad4702e10ad3b2360d47f2fc92
chbarbosa <carloshenriquefb@gmail.com>
2023-09-27 18:10:24 -0400

    componente basico adicionado.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'iterative-fantasy-1';
   enteringCastle: boolean = false;
+  night: boolean = localStorage.getItem("night") === "true";
+
+  goTonight() {
+    alert("ir à noite!");
+    this.night = true;
+    localStorage.setItem("night", "true");
+  }
+  goNow() {    
+    if(confirm("Confirma?")) {
+      alert("ir imediatamente!");
+      this.night = false;
+      localStorage.setItem("night", "false");
+    } 
+  }
 
   goCastle() {
     this.enteringCastle = true;
