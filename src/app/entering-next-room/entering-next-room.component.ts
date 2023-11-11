4e51929f753db2569ca96ad286e58c3bf87dc271
chbarbosa <carloshenriquefb@gmail.com>
2023-11-10 21:01:43 -0500

    first main history ready

--- src/app/entering-next-room/entering-next-room.component.ts
+++ src/app/entering-next-room/entering-next-room.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./entering-next-room.component.css']
 })
 export class EnteringNextRoomComponent {
+  private readonly actionRef: string = "nextRoomAction";
+  hasRun(): any {
+    return  localStorage.getItem(this.actionRef) === "run";
+  }
+  hasUsedSword(): any {
+    return  localStorage.getItem(this.actionRef) === "sword";
+  }
+  hasUsedBesta(): any {
+    return  localStorage.getItem(this.actionRef) === "besta";
+  }
+  run() {
+    localStorage.setItem(this.actionRef, "run");
+  }
+  useSword() {
+    localStorage.setItem(this.actionRef, "sword");
+  }
+  useBesta() {
+    localStorage.setItem(this.actionRef, "besta");
+  }
+  hasNoDecision(): any {
+    return localStorage.getItem(this.actionRef) == undefined;
+  }
 
 }
